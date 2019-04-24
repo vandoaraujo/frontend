156bb24a703939bf3cdfcbfab56851cb2d45f63f
Vando Araujo <tecladista.vando@gmail.com>
2019-04-23 22:19:15 -0300

    news components

--- src/user/UserCrud.jsx
+++ src/user/UserCrud.jsx
@@ -19,7 +19,28 @@ export default class UserCrud extends Component {
     state = { ...initialState }
 
     clear() {
-        
+        this.setState( { user: initialState.user} )
+    }
+
+    save() {
+        const user  = this.state.user
+        const method = user.id ? 'put' : 'post'
+        const url = user.id ? `${baseUrl}/${user.id}` : baseUrl
+        axios[method](url, user)
+            .then(resp => {
+                const list = this.getUpdatedList(resp.data)
+            })
+    }
+
+    /**
+     * 
+     * Removo usuario da lista e adiciono o novo criando uma lista nova
+     * @param user 
+     */
+    getUpdatedList(user) {
+        const list = tis.state.list.filter(u => u.id !== user.id)
+        list.unshift(user)
+        return list
     }
 
     render() {
