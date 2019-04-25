45fe8f7b59aea4d22bf058ce326318755915d7e4
Vando Araujo <tecladista.vando@gmail.com>
2019-04-24 23:12:23 -0300

    componnte save

--- src/user/UserCrud.jsx
+++ src/user/UserCrud.jsx
@@ -60,20 +60,48 @@ export default class UserCrud extends Component {
                     <div className="col-12 col-md-6">
                         <div className="form-group">
                             <label>Nome</label>
-                            <input type="text" className="form-controle"
+                            <input type="text" className="form-control"
                                 name="name" value={this.state.user.name}
-                                onChange={e => this.updateField(e)}/>
+                                onChange={e => this.updateField(e)}
+                                placeholder="Digite o nome..."/>
+                        </div>
+                    </div>
+
+                    <div className="col-12 col-md-6">
+                        <div className="form-group">
+                            <label>E-mail</label>
+                            <input type="text" className="form-control"
+                            name="email"
+                            value={this.state.user.email}
+                            onChange={e => this.updateField(e)}
+                            placeholder="Digite o email..."
+                            />
                         </div>
                     </div>
                 </div>
+                <hr />
+                <div className="row">
+                    <div className="col-12 d-flex justify-content-end">
+                        <button className="btn btn-primary"
+                            onClick={e => this.save(e)}>
+                            Salvar
+                        </button>
+
+                        <button className="btn btn-secondary ml-2"
+                            onClick={e => this.clear(e)}>
+                            Cancelar
+                        </button>
+                    </div>   
+                </div>
             </div>
+            
         )
     }
 
     render() {
         return (
             <Main {...headerProps}>
-
+                {this.renderForm()}
             </Main>
         )
     }
