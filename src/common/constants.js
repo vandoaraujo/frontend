442814bf3eb905468e77a70332e49b9578574ce0
Vando Ribeiro de Araujo <tecladista.vando@gmail.com>
2019-12-23 13:07:51 -0300

    melhorias

--- src/common/constants.js
+++ src/common/constants.js
@@ -5,8 +5,4 @@ module.exports = Object.freeze({
     API_BASE_BACKEND_SERVER: 'https://cadastromembrosibbback.herokuapp.com/',
     PERFIL_SUPER_USER: 777,
     APP_SECRET_KEY : process.env.APP_SECRET_KEY || '930dca47b14ba687cdcb62469a3c95b5'
-    // MEMBRO_ATIVO: 1,
-    // PERFIL_SUPER_USER: process.env.SUPER_USER || 'vandoaraujo',
-    // PERFIL_CONSULTA_USER: 1,
-    // SALTOS_HASH: 10
 });
